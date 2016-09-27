9aa3c077767795fd0aca71c2c6a30c9dc0adf6c3
Catie Myers-Wood <cmyerswood@gmail.com>
2016-09-27 18:13:05 -0400

    Final project progress

--- final-project/version-2/project-files/js/script.js
+++ final-project/version-2/project-files/js/script.js
@@ -1,4 +1,23 @@
-/*Back to top button*/
-	$('.top').click(function() {
-		$("html, body").animate({scrollTop: 650}, 2800);
-	});
+// /*Back to top button*/
+// 	$('.top').click(function() {
+// 		$("html, body").animate({scrollTop: 650}, 2800);
+// 	});
+
+$(document).ready(function () {
+
+	$(nav).hover(
+        function (event) {
+            $(this).find(".hideshow").show();
+            $(this).find(".hideshow").css("background-color", "#808080");
+
+        },
+        function (event) {
+            $(this).find(".hideshow").hide();
+            $(this).find(".hideshow").css("background-color", "#fff");
+        }
+     );
+
+
+
+
+});
